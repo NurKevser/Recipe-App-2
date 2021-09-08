0c18565711e4628ee5b86e02940ec50c0e5c50b8
NurKevser <nurkevserkadiroglu@gmail.com>
2021-09-08 23:39:25 +0300

    initiliazed detail folder

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,19 +1,18 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import React from 'react'
-import Navbar from '../components/navbar/Navbar';
-import Home from '../pages/home/Home';
-import About from '../pages/about/About'
-
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "../components/navbar/Navbar";
+import About from "../pages/about/About";
+import Home from "../pages/home/Home";
+import Details from "../pages/details/Details";
 function AppRouter() {
-    return (
-        <Router>
-            <Navbar />
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/about" exact component={About} />
-            </Switch>
-        </Router>
-    )
+  return (
+    <BrowserRouter>
+        <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/details" exact component={Details} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
-
-export default AppRouter
+export default AppRouter;
