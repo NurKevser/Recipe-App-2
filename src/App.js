32456f6388ef1a906c900543389bc15d3f24253e
NurKevser <nurkevserkadiroglu@gmail.com>
2021-09-08 21:52:36 +0300

    navbar and router completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppRouter from './router/AppRouter'
+
+const APP_KEY = "5909072aaf43c1a4a50f2d44352a9ed9"
+
+const App = () => {
+  return (
+    <AppRouter />
+
+  )
+
+};
+export default App;
