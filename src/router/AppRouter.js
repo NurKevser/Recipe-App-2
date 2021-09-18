57c05b5c5eab6f3fb9e1f9c00ee821fc30551966
NurKevser <nurkevserkadiroglu@gmail.com>
2021-09-19 00:27:49 +0300

    user authentication

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -4,17 +4,36 @@ import About from "../pages/about/About";
 import Home from "../pages/home/Home";
 import Details from "../pages/details/Details";
 import Login from "../pages/login/Login";
+import PrivateRouter from './PrivateRouter';
+import { useState } from "react";
+
+
 function AppRouter() {
+  const [isAuth, setIsAuth] = useState(false);
+
+  const AuthContainer = () => {
+    <div>
+      <Navbar />
+      <PrivateRouter isAuth={isAuth} exact path="/" component={Home} />
+      <PrivateRouter isAuth={isAuth} exact path="/about" component={About} />
+      <PrivateRouter isAuth={isAuth} exact path="/details" component={Details} />
+    </div>
+  }
   return (
     <BrowserRouter>
-        <Navbar />
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={About} />
-        <Route path="/details" exact component={Details} />
-        <Route path="/login" exact component={Login} />
+       <Route path="/login"
+       exact
+       component={() => <Login setIsAuth={setIsAuth}/>}
+       />
+       <Route component={AuthContainer}/>
       </Switch>
     </BrowserRouter>
   );
 }
 export default AppRouter;
+
+{/* <Route path="/" exact component={Home} />
+<Route path="/about" exact component={About} />
+<Route path="/details" exact component={Details} />
+<Route path="/login" exact component={Login} /> */}
