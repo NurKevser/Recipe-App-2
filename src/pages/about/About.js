d1b7853810e853ba62a13cca398a3cf3cae4ca35
NurKevser <nurkevserkadiroglu@gmail.com>
2021-09-18 23:47:22 +0300

    completed about page

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -9,9 +9,25 @@ import codingSvg from '../../assets/coding.svg';
 
 const About = () => {
     return (
-        <div>
-            <h2>About</h2>
-        </div>
+        <AboutContainer>
+            <StyledImage src={codingSvg} alt="coding"/>
+            <HeaderContainer>
+                <h1>
+                    About Software Developer <span>Nukeka</span>
+                </h1>
+            </HeaderContainer>
+            <InfoContainer>
+                <h1>I'm Nur Kevser</h1>
+                <h2>I'm currently learning Full-Stack Development Languages.</h2>
+                <h2>
+                    I've already known JS, ReactJS, PYTHON, SQL.
+                </h2>
+                <h2>
+                    <a href="mailto:nukeka07@gmail.com">Send Email</a> :
+                    nukeka07@gmail.com
+                </h2>
+            </InfoContainer>
+        </AboutContainer>
     );
 
 };
